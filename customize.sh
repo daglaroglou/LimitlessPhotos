ec2af8cc0beb769bb766ad12b0f7412dc0ecfbd0
Christos Daglaroglou <christos.daglaroglou@gmail.com>
2025-05-17 15:37:52 +0300

    Add files via upload

--- customize.sh
+++ customize.sh
@@ -1,32 +1,66 @@
-for i in /system/product/etc/sysconfig/*; do
-    file=$i
-    file=${file/\/system\/product\/etc\/sysconfig\//}
-    if [ ! -z "$(grep PIXEL_2020_ $i)" ] || [ ! -z "$(grep PIXEL_2021_ $i)" ] || [ ! -z "$(grep PIXEL_2019_PRELOAD $i)" ] || [ ! -z "$(grep PIXEL_2018_PRELOAD $i)" ] || [ ! -z "$(grep PIXEL_2017_PRELOAD $i)" ] || [ ! -z "$(grep PIXEL_2022_ $i)" ]; then
-        [ ! -f $MODPATH/system/product/etc/sysconfig/$file ] && cat /system/product/etc/sysconfig/$file | grep -v PIXEL_2020_ | grep -v PIXEL_2021_ | grep -v PIXEL_2022_ | grep -v PIXEL_2018_PRELOAD | grep -v PIXEL_2019_PRELOAD >$MODPATH/system/product/etc/sysconfig/$file
-    fi
-done
-for i in /system/etc/sysconfig/*; do
-    file=$i
-    file=${file/\/system\/etc\/sysconfig\//}
-    if [ ! -z "$(grep PIXEL_2020_ $i)" ] || [ ! -z "$(grep PIXEL_2021_ $i)" ] || [ ! -z "$(grep PIXEL_2019_PRELOAD $i)" ] || [ ! -z "$(grep PIXEL_2018_PRELOAD $i)" ] || [ ! -z "$(grep PIXEL_2017_PRELOAD $i)" ] || [ ! -z "$(grep PIXEL_2022_ $i)" ]; then
-        [ ! -f $MODPATH/system/product/etc/sysconfig/$file ] && cat /system/etc/sysconfig/$file | grep -v PIXEL_2020_ | grep -v PIXEL_2021_ | grep -v PIXEL_2022_ | grep -v PIXEL_2018_PRELOAD | grep -v PIXEL_2019_PRELOAD | grep -v PIXEL_2017_PRELOAD >$MODPATH/system/etc/sysconfig/$file
-    fi
-done
-if [ -d /data/adb/modules/PixelifyPhotos/system/product/etc/sysconfig ]; then
-    for i in /data/adb/modules/PixelifyPhotos/system/product/etc/sysconfig/*; do
-        file=$i
-        file=${file/\/data\/adb\/modules\/PixelifyPhotos\/system\/product\/etc\/sysconfig\//}
-        if [ ! -f $MODPATH/system/product/etc/sysconfig/$file ]; then
-            cp -f /data/adb/modules/PixelifyPhotos/system/product/etc/sysconfig/$file $MODPATH/system/product/etc/sysconfig/$file
+# /bin/bash
+# Initial method: @cuynu
+# https://gitlab.com/cuynu/gphotos-unlimited-zygisk
+# Modified by: @daglaroglou
+
+ui_print "» Detecting installation environment..."
+if [ -d "/data/adb/ksu" ] || [ -f "/data/adb/ksud" ]; then
+  ui_print "✓ KernelSU detected"
+else
+  ui_print "✓ Magisk detected"
+fi
+ui_print ""
+ui_print "» Checking device compatibility..."
+sleep 0.5
+if [ "$(getprop ro.build.version.sdk)" -lt "30" ]; then
+  ui_print "! Warning: This module works best on Android 11+ (SDK 30+)"
+  ui_print "  Your device is running $(getprop ro.build.version.release)"
+  ui_print "  Some features may not work properly"
+  sleep 1
+else
+  ui_print "✓ Android version: $(getprop ro.build.version.release)"
+fi
+ui_print ""
+ui_print "» Patching Google Photos..."
+
+PIXEL_PATTERNS="PIXEL_2017_PRELOAD\|PIXEL_2018_PRELOAD\|PIXEL_2019_PRELOAD\|PIXEL_2020_\|PIXEL_2021_\|PIXEL_2022_"
+
+process_sysconfig() {
+    local src_dir="$1"
+    local dest_dir="$2"
+    
+    mkdir -p "$MODPATH/$dest_dir"
+    
+    for file in "$src_dir"/*; do
+        [ -f "$file" ] || continue
+        local filename=$(basename "$file")
+        local dest_file="$MODPATH/$dest_dir/$filename"
+        if grep -q "$PIXEL_PATTERNS" "$file"; then
+            [ ! -f "$dest_file" ] && grep -v "$PIXEL_PATTERNS" "$file" > "$dest_file"
+        fi
+    done
+}
+
+process_sysconfig "/system/product/etc/sysconfig" "system/product/etc/sysconfig"
+process_sysconfig "/system/etc/sysconfig" "system/etc/sysconfig"
+
+OLD_MODULE="/data/adb/modules/LimitlessPhotos"
+if [ -d "$OLD_MODULE" ]; then
+    for dir in "system/product/etc/sysconfig" "system/etc/sysconfig"; do
+        if [ -d "$OLD_MODULE/$dir" ]; then
+            mkdir -p "$MODPATH/$dir"
+            for file in "$OLD_MODULE/$dir"/*; do
+                [ -f "$file" ] || continue
+                filename=$(basename "$file")
+                [ ! -f "$MODPATH/$dir/$filename" ] && cp -f "$file" "$MODPATH/$dir/$filename"
+            done
         fi
     done
 fi
-if [ -d /data/adb/modules/PixelifyPhotos/system/etc/sysconfig ]; then
-    for i in /data/adb/modules/PixelifyPhotos/system/etc/sysconfig/*; do
-        file=$i
-        file=${file/\/data\/adb\/modules\/PixelifyPhotos\/system\/etc\/sysconfig\//}
-        if [ ! -f $MODPATH/system/etc/sysconfig/$file ]; then
-            cp -f /data/adb/modules/PixelifyPhotos/system/etc/sysconfig/$file $MODPATH/system/etc/sysconfig/$file
-        fi
-    done
-fi
+
+ui_print ""
+ui_print "» Installing module to $ENV_TYPE..."
+sleep 0.5
+ui_print "✓ Module installed successfully!"
+ui_print ""
+ui_print "********************************"
